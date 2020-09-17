d044c8a196deb39d3bb48b7f9327b9e976562d43
Stefan Roks <stefanroks@gmail.com>
2020-09-17 15:21:14 +0200

    revamp

---
+++ Deploy-SolrAzureAppService.ps1
@@ -0,0 +1,106 @@
+Param(
+    $solrVersion
+)
+
+$solrName = "solr-$solrVersion"
+
+Write-Output 'Setting Security to TLS 1.2'
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+Write-Output 'Prevent the progress meter from trying to access the console'
+$global:progressPreference = 'SilentlyContinue'
+
+$siteRoot = "D:\home\site\wwwroot"
+
+Write-Output "Downloading Solr $solrVersion zip to D:\home\site"
+
+$downloadSource = "https://archive.apache.org/dist/lucene/solr/$solrVersion/$solrName.zip"
+Invoke-WebRequest -Uri $downloadSource -UseBasicParsing -OutFile "..\solr.zip"
+
+Write-Output "Expanding Solr zip at D:\home\site directory as D:\home\site\$solrName"
+Expand-Archive "..\solr.zip" -DestinationPath "..\"
+
+Write-Output "Copying contents of D:\home\site\$solrName to D:\home\site\wwwroot"
+
+xcopy "..\$solrName\*" "..\wwwroot" /S /Y
+
+Write-Output 'Copy web.config from repository to D:\home\site\wwwroot'
+xcopy web.config ..\wwwroot /Y
+
+Write-Output 'Copy default configset as sitecore'
+$usingDefault = $false
+if(Test-Path "..\wwwroot\server\solr\configsets\_default" -PathType Any){
+	$usingDefault = $true
+	xcopy "..\wwwroot\server\solr\configsets\_default\*.*" "..\wwwroot\server\solr\configsets\sitecore\*" /s/h/e/k/f/c/Y
+}
+else{
+	xcopy "..\wwwroot\server\solr\configsets\basic_configs\*.*" "..\wwwroot\server\solr\configsets\sitecore\*" /s/h/e/k/f/c/Y
+}
+
+
+Write-Output 'Modifying uniqueKey of sitecore configset'
+
+$xml = New-Object XML
+$path = "..\wwwroot\server\solr\configsets\sitecore\conf\managed-schema"
+$xml.Load($path)
+
+$uniqueKey =  $xml.SelectSingleNode("//uniqueKey")
+$uniqueKey.InnerText = "_uniqueid"
+
+$field = $xml.CreateElement("field")
+$field.SetAttribute("name", "_uniqueid")
+$field.SetAttribute("type", "string")
+$field.SetAttribute("indexed", "true")
+$field.SetAttribute("required", "true")
+$field.SetAttribute("stored", "true")
+
+$xml.DocumentElement.AppendChild($field)
+
+$xml.Save($path)
+
+$sitecoreCores = @(
+	"sitecore_analytics_index", 
+    "sitecore_core_index", 
+    "sitecore_fxm_master_index", 
+	"sitecore_fxm_web_index", 
+	"sitecore_list_index", 
+    "sitecore_marketing_asset_index_master", 
+    "sitecore_marketing_asset_index_web", 
+	"sitecore_marketingdefinitions_master", 
+	"sitecore_marketingdefinitions_web", 
+    "sitecore_master_index", 
+	"sitecore_suggested_test_index", 
+	"sitecore_testing_index", 
+	"sitecore_web_index", 
+    "social_messages_master", 
+	"social_messages_web"
+)
+
+foreach ($coreName in $sitecoreCores) {
+	Write-Output "Creating $coreName index"
+	New-Item "..\wwwroot\server\solr\" -Name "$coreName" -ItemType "directory"
+	New-Item "..\wwwroot\server\solr\$coreName" -Name "data" -ItemType "directory"
+	xcopy "..\wwwroot\server\solr\configsets\sitecore\conf\*" "..\wwwroot\server\solr\$coreName\conf\*" /S /Y
+	New-Item "..\wwwroot\server\solr\$coreName\core.properties"
+	Set-Content "..\wwwroot\server\solr\$coreName\core.properties" "name=$coreName`r`nupdate.autoCreateFields=false`r`ndataDir=data"
+}
+
+$xdbCores = @(
+	"xdb", 
+    "xdb_rebuild"
+)
+
+foreach ($coreName in $xdbCores) {
+	Write-Output "Creating $coreName index"
+	New-Item "..\wwwroot\server\solr\" -Name "$coreName" -ItemType "directory"
+	New-Item "..\wwwroot\server\solr\$coreName" -Name "data" -ItemType "directory"
+
+	if($usingDefault){
+		xcopy "..\wwwroot\server\solr\configsets\_default\conf\*" "..\wwwroot\server\solr\$coreName\conf\*" /S /Y
+	}
+	else{
+		xcopy "..\wwwroot\server\solr\configsets\basic_configs\conf\*" "..\wwwroot\server\solr\$coreName\conf\*" /S /Y
+	}
+
+	New-Item "..\wwwroot\server\solr\$coreName\core.properties"
+	Set-Content "..\wwwroot\server\solr\$coreName\core.properties" "name=$coreName`r`ndataDir=data"
+}
---
+++ README.md
@@ -0,0 +1,33 @@
+## Disclaimer
+
+This technique is **not recommended** for production instances. The ideal use-case is to quickly spin up sandbox environments or non-prod environments. Once this deployment is complete, the standard Sitecore install process can be followed with no tinkering of Solr: Azure Quick Start ARM templates or via Sitecore Experience Cloud Azure resource.
+
+# Solr-AzureAppService for Sitecore
+
+[![Deploy to Azure](https://azuredeploy.net/deploybutton.png)](https://azuredeploy.net/)
+
+Install Solr as an App Service in about 10 minutes preconfigured for Sitecore 8.2u7+. Click the button above and follow the prompts. Be sure to select the proper Solr Version for your Sitecore installation (https://kb.sitecore.net/articles/227897). Once this deployment completes, all prerequisites for Sitecore are in place. You may run quick start templates or install directly from "Sitecore Experience Cloud" Azure resource and provide the Solr link, e.g. https://myappservice.dev.azure.net/solr. Full credit goes to Dan Cruickshank. His guide was used as the basis for this one-click deployment:
+
+https://getfishtank.ca/blog/installing-solr-app-service-in-sitecore-azure-paas
+
+This script sets the `httpsOnly` property to `true` by default, but it can be modified in the parameters (in the prompts).
+
+## Deployment Summary
+
+1. \[Deploy to Azure\] button looks for `azuredeploy.json` file at repo root
+2. `azuredeploy.json` deploys resources. Notable steps listed below:
+    1. `solrVersion` parameter is added as an `appSetting` to the web app. This is required to parameterize it when calling `Deploy-SolrAzureAppService.ps1`
+    2. `web > config` resource. Sets `javaVersion` to 1.8 which supports all (?) Solr versions
+    3. `sourcecontrols` resource. This is the brains of the operation. It deploys THIS repository to your web app.
+        1. With this resource deployment, if a file named `.deployment` exists at the repo root, it is executed once the repo has been cloned to your web app
+3. The `.deployment` file calls `Deploy-SolrAzureAppService.ps1` with the Solr version as a parameter (parameters are only accessible from the `.deployment` file). 
+4. `Deploy-SolrAzureAppService.ps1` is where the majority of work occurs. This is where Dan's manual work was implemented in an automated fashion
+    1. This script downloads and extracts the desired Solr version to the web root
+    2. It also copies the `web.config` from the repo to the web root
+
+## View deployment logs
+
+To see the deployment logs, find your deployed App Service in Azure, then in the left-hand navigation select `Development Tools > Advanced Tools (Kudu)`, then click `Go` to launch Kudu. Once Kudu launches in a new tab, from the primary navigation select `Tools > Zip Push Deploy`. Once this page loads, expand the deployment steps at the bottom to see the log output and any errors.
+
+### Resources
+- https://github.com/projectkudu/kudu/wiki/Customizing-deployments
